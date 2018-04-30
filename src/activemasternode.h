96592a4637ed0bed213f8b22f0d047a0fd38834c
8nyaha8 <35270887+8nyaha8@users.noreply.github.com>
2018-05-01 00:18:13 +0300

    Fix Copyright

--- src/activemasternode.h
+++ src/activemasternode.h
@@ -1,5 +1,7 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-2012 The Bitcoin developers
+// Copyright (c) 2009-2016 The Bitcoin developers
+// Copyright (c) 2009-2018 The Pprfnk developers
+// Copyright (c) 2018 The Pprfnk developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 #ifndef ACTIVEMASTERNODE_H
--- src/pprfnk-cli.cpp
+++ src/pprfnk-cli.cpp
@@ -1,6 +1,7 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-2015 The Bitcoin developers
-// Copyright (c) 2009-2015 The Pprfnk developers
+// Copyright (c) 2009-2016 The Bitcoin developers
+// Copyright (c) 2009-2018 The Pprfnk developers
+// Copyright (c) 2018 The Pprfnk developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
